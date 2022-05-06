fd02b6b2bd1538d3e4227618e9c175dfeed99a0a
Osée Masuaku <40447959+oseeshogun@users.noreply.github.com>
2022-05-06 11:22:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+function valMinMax(arr){
+
+  if (!arr.length) return "0 0";
+  
+  let sums = [];
+
+  for (let i = 0; i < arr.length; i++){
+    let arrCopy = [...arr];
+    arrCopy.splice(i, 1);
+    const sum = arrCopy.reduce((a, b) => a + b, 0);
+    sums.push(sum);
+  }
+
+  const min = Math.min(...sums);
+  const max = Math.max(...sums);
+
+  return min + " " + max;
+}
+function matriceCaree(arr){
+  const x = arr.length;
+  let sumDiagonalOne = 0;
+  let sumDiagonalTwo = 0;
+
+  for (let i = 0; i < x; i++){
+    const diagonalOneItem = arr[i][i];
+    const diagonalTwoItem = arr[i][x-i-1];
+
+    sumDiagonalOne += diagonalOneItem;
+    sumDiagonalTwo += diagonalTwoItem;
+  }
+
+  return Math.abs(sumDiagonalOne-sumDiagonalTwo);
+}
+function camelCase(str){
+  if (!str.trim()) return 0;
+  let count = 1;
+  
+  for (let i = 0; i < str.length; i++){
+    const letter = str[i];
+    const isUpperCase = letter == letter.toUpperCase();
+
+    if (isUpperCase) count++;
+  }
+
+  return count;
+}
+function arcadeRank(top_scores, jeu){
+
+  const getRank = (x) => {
+    let topScoresCopy = [...new Set([...top_scores, , x])];
+    topScoresCopy.sort((a, b) => b - a);
+    return topScoresCopy.indexOf(x) + 1;
+  }
+
+  let ranks = [];
+
+  for (let i = 0; i < jeu.length;i++){
+    const score = jeu[i];
+
+    const rank = getRank(score);
+
+    const minRank = Math.min(...ranks);
+
+    ranks.push(minRank < rank ? minRank : rank);
+  }
+
+  return ranks;
+}
+
+
+module.exports = { valMinMax, matriceCaree, camelCase, arcadeRank };
